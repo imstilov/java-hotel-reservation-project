ea1772dd8aadc4d5532413e551e0ff3a82958339
stilovv <ai9974929@gmail.com>
2026-03-25 20:01:38 +0100

    init commit

---
+++ src/main/java/com/stilov/springboot_practice_2503/SpringbootPractice2503Application.java
@@ -0,0 +1,13 @@
+package com.stilov.springboot_practice_2503;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPractice2503Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPractice2503Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/stilov/springboot_practice_2503/SpringbootPractice2503ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stilov.springboot_practice_2503;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPractice2503ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

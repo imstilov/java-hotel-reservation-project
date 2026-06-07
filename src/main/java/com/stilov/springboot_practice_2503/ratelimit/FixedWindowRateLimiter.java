fc87c2add17d4e4bd4f9128568bd3394038fca28
vitalii <ai9974929@gmail.com>
2026-06-07 16:05:51 +0200

    v 2.4 added simple Rate Limiter with Redis(Fixed Window pattern)

---
+++ src/main/java/com/stilov/springboot_practice_2503/ratelimit/FixedWindowRateLimiter.java
@@ -0,0 +1,32 @@
+package com.stilov.springboot_practice_2503.ratelimit;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@AllArgsConstructor
+public class FixedWindowRateLimiter {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public boolean allowRequest(
+            String clientId,
+            int limit,
+            Duration windowSize
+    ){
+        long windowIndex = System.currentTimeMillis() / windowSize.toMillis();
+        String key = String.format("rate:%s:%s", clientId, windowIndex);
+
+        Long countHits = stringRedisTemplate.opsForValue()
+                .increment(key);
+
+        if(countHits != null && countHits == 1){
+            stringRedisTemplate.expire(key, windowSize);
+        }
+
+        return countHits != null && countHits <= limit;
+    }
+}
